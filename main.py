094cfafcb46f62b6f8262f3a1d80099bb75b89e5
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2024-02-13 16:43:05 +0900

    fix: add use_salts default parameter to the crack_sha1_hash function

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import password_cracker
 from unittest import main
 
 cracked_password1 = password_cracker.crack_sha1_hash(
-    "fbbe7e952d1050bfb09dfdb71d4c2ff2b3d845d2")
+    "fbbe7e952d1050bfb09dfdb71d4c2ff2b3d845d2", False)
 print(cracked_password1)
 
 cracked_password2 = password_cracker.crack_sha1_hash(
-    "dcc466796201f7232b22a03781110a8871fd038c", use_salts=True)
+    "dcc466796201f7232b22a03781110a8871fd038c", True)
 print(cracked_password2)
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+main(module = "test_module", exit = False)
